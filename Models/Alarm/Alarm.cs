339fcd212fd2fc01af42b244083a2c08d9bc9c77
ASPL-Yantao <tao.yan@alliedsolutions.com.cn>
2023-09-25 13:42:58 +0800

    Update Historical Alarm

--- Models/Alarm/Alarm.cs
+++ Models/Alarm/Alarm.cs
@@ -72,9 +72,45 @@ namespace Models
     }
 
     [Table("tHistoricalAlarm")]
-    public class HistoricalAlarm : Alarm
+    public class HistoricalAlarm 
     {
+        [Key]
         [Column("ID")]
         public int HisID { get; set; }
+        [Column("ALM_NATIVETIMEIN")]
+        public DateTime StartTime { get; set; }
+        [Column("ALM_NATIVETIMELAST")]
+        public DateTime EndTime { get; set; }
+        [Column("ALM_PHYSLNODE")]
+        public string NodeName { get; set; }
+        [Column("ALM_TAGNAME")]
+        public string TagName { get; set; }
+        [Column("ALM_VALUE")]
+        public string Value { get; set; }
+        [Column("ALM_MSGTYPE")]
+        public string MessageType { get; set; }
+        [Column("ALM_DESCR")]
+        public string Description { get; set; }
+        [Column("ALM_ALMPRIORITY")]
+        public string Priority { get; set; }
+        [Column("ALM_ALMSTATUS")]
+        public string Status { get; set; }
+        [Column("ALM_ALMAREA")]
+        public string Area { get; set; }
+        [Column("ALM_OPNAME")]
+        public string OperatorName { get; set; }
+        [Column("ALM_OPFULLNAME")]
+        public string FullOperatorName { get; set; }
+    }
+
+    public class HistoricalStatisticalAlarm
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public int Count { get; set; }
+        public string TagName { get; set; }
+        public string Description { get; set; }
+
     }
 }
